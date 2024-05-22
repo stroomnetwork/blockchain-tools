7cfc963985838c5943f9ef45df54cac75663552d
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2024-05-22 16:38:56 +0300

    refactor BTCDepositAddressDeriver (#6)
    * refactor BTCDepositAddressDeriver
    
    * refactor BTCDepositAddressDeriver
    
    * pick network from env
    
    * refactor: network prefix function

--- script/SetSeed.s.sol
+++ script/SetSeed.s.sol
@@ -9,23 +9,32 @@ import {BTCDepositAddressDeriver} from "../src/BTCDepositAddressDeriver.sol";
 
 contract SetSeed is Script {
     function run() external {
+        // get contract address
         address contractAddress = DevOpsTools.get_most_recent_deployment(
             "BTCDepositAddressDeriver",
             block.chainid
         );
         console.log("contractAddress", contractAddress);
 
+        // get first validators' joint pubkey
         string memory btcAddr1 = vm.envString("BTC_ADDR1");
         console.log("BTC_ADDR1:", btcAddr1);
 
+        // get second validators' joint pubkey
         string memory btcAddr2 = vm.envString("BTC_ADDR2");
         console.log("BTC_ADDR2:", btcAddr2);
+
+        // get network
+        uint network = vm.envUint("Network");
+        console.log("Network:", network);
 
         BTCDepositAddressDeriver deriver = BTCDepositAddressDeriver(
             contractAddress
         );
+
+        // set validators' pubkeys and network prefix
         vm.startBroadcast();
-        deriver.setSeed(btcAddr1, btcAddr2, "bcrt");
+        deriver.setSeed(btcAddr1, btcAddr2, BTCDepositAddressDeriver.BitcoinNetwork(network));
         vm.stopBroadcast();
     }
 }
