29be480092a20e574a6da562a73be2bf01ac8927
Viacheslav <s.zhygulin@gmail.com>
2024-05-22 14:04:24 +0300

    refactor BTCDepositAddressDeriver

--- src/BTCDepositAddressDeriver.sol
+++ src/BTCDepositAddressDeriver.sol
@@ -6,22 +6,35 @@ import {Deriver} from "./Deriver.sol";
 import {Bech32m} from "./Bech32m.sol";
 
 error SeedWasNotSetYet();
-
+error UnsupportedBtcAddress(string btcAddress);
 error CannotParseBtcAddress(
     string btcAddress,
     string hrp,
     Bech32m.DecodeError err
 );
-error UnsupportedBtcAddress(string btcAddress);
+
+// Types of Bitcoin Network
 
 contract BTCDepositAddressDeriver {
+
+    enum BitcoinNetwork {
+        TESTNET,
+        MAINNET,
+        REGTEST
+    }
+
     event SeedChanged(string btcAddr1, string btcAddr2, string hrp);
 
     bool public wasSeedSet;
+
+    // Validators' joint pubkeys in format of taproot addresses
     string public btcAddr1;
     string public btcAddr2;
+
+    // Bitcoin address's network prefix
     string public networkHrp;
 
+    // Validators' pubkey both coordinates deconstructed from taproot addresses
     uint256 public p1x;
     uint256 public p1y;
     uint256 public p2x;
@@ -31,21 +44,55 @@ contract BTCDepositAddressDeriver {
         wasSeedSet = false;
     }
 
+    // Set Validators' joint pubkeys and network prefix, must be called after contract deployment
+    function setSeed(
+        string calldata _btcAddr1,
+        string calldata _btcAddr2,
+        BitcoinNetwork _network
+    ) public virtual {
+
+        string memory _hrp;
+
+        if (_network == BitcoinNetwork.TESTNET) {
+            _hrp = 'tb';
+        } else if (_network == BitcoinNetwork.MAINNET) {
+            _hrp = 'bc';
+        } else if (_network == BitcoinNetwork.REGTEST) {
+            _hrp = 'brct';
+        }
+
+        networkHrp = _hrp;
+
+        (p1x, p1y) = parseBTCTaprootAddress(_hrp, _btcAddr1);
+        (p2x, p2y) = parseBTCTaprootAddress(_hrp, _btcAddr2);
+
+        btcAddr1 = _btcAddr1;
+        btcAddr2 = _btcAddr2;
+
+        wasSeedSet = true;
+        emit SeedChanged(_btcAddr1, _btcAddr2, _hrp);
+    }
+
+    // Derive pubkey's (x,y) coordinates from taproot address
     function parseBTCTaprootAddress(
-        string calldata hrp,
-        string calldata addr
+        string memory _hrp,
+        string calldata _bitcoinAddress
     ) public pure returns (uint256, uint256) {
+
         (uint8 witVer, bytes memory witProg, Bech32m.DecodeError err) = Bech32m
-            .decodeSegwitAddress(bytes(hrp), bytes(addr));
+            .decodeSegwitAddress(bytes(_hrp), bytes(_bitcoinAddress));
+
         if (err != Bech32m.DecodeError.NoError) {
-            revert CannotParseBtcAddress(addr, hrp, err);
+            revert CannotParseBtcAddress(_bitcoinAddress, _hrp, err);
         }
         if (witVer != 1 || witProg.length != 32) {
-            revert UnsupportedBtcAddress(addr);
+            revert UnsupportedBtcAddress(_bitcoinAddress);
         }
+
         uint256 x = uint256(bytes32(witProg));
+
         if (x == 0 || x >= Deriver.PP) {
-            revert UnsupportedBtcAddress(addr);
+            revert UnsupportedBtcAddress(_bitcoinAddress);
         }
 
         uint256 y = Deriver.liftX(x);
@@ -53,26 +100,15 @@ contract BTCDepositAddressDeriver {
         return (x, y);
     }
 
-    function setSeed(
-        string calldata _btcAddr1,
-        string calldata _btcAddr2,
-        string calldata _hrp
-    ) public virtual {
-        networkHrp = _hrp;
-        (p1x, p1y) = parseBTCTaprootAddress(_hrp, _btcAddr1);
-        (p2x, p2y) = parseBTCTaprootAddress(_hrp, _btcAddr2);
-        btcAddr1 = _btcAddr1;
-        btcAddr2 = _btcAddr2;
-        wasSeedSet = true;
-        emit SeedChanged(_btcAddr1, _btcAddr2, _hrp);
-    }
-
+    // Get users' Bitcoin deposit address from user's Ethereum address
     function getBTCDepositAddress(
         address ethAddr
     ) public view returns (string memory) {
+
         if (!wasSeedSet) {
             revert SeedWasNotSetYet();
         }
+
         return
             Deriver.getBtcAddressFromEth(
                 p1x,
