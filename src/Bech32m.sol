758823cb2ed56042b6770fe8ed56bd578767f0c0
Viacheslav <s.zhygulin@gmail.com>
2024-05-28 17:39:45 +0300

    change license

--- src/Bech32m.sol
+++ src/Bech32m.sol
@@ -1,5 +1,5 @@
+// SPDX-License-Identifier: MIT
 // https://github.com/gregdhill/bech32-sol/blob/master/src/Bech32.sol
-// TODO(mkl): what License?
 // based on https://github.com/bitcoin/bips/blob/master/bip-0350.mediawiki
 // https://github.com/sipa/bech32/blob/master/ref/python/segwit_addr.py
 pragma solidity ^0.8.18;
