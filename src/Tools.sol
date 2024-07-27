21e419eff05cfab283a4fb63de4f4f1a67532a2c
Mykola Sakhno <sakhno.m@gmail.com>
2024-07-27 14:43:45 +0300

    make functions internal

--- src/Tools.sol
+++ src/Tools.sol
@@ -2,22 +2,19 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-
 library Tools {
     // https://www.educative.io/answers/how-to-compare-two-strings-in-solidity
     function areStringsEqual(
         string memory str1,
         string memory str2
-    ) public pure returns (bool) {
+    ) internal pure returns (bool) {
         return
             keccak256(abi.encodePacked(str1)) ==
             keccak256(abi.encodePacked(str2));
     }
 
     // https://stackoverflow.com/questions/69551020/trying-to-convert-address-string-to-type-address-in-solidity
-    function fromHexChar(uint8 c) public pure returns (uint8) {
+    function fromHexChar(uint8 c) internal pure returns (uint8) {
         if (bytes1(c) >= bytes1("0") && bytes1(c) <= bytes1("9")) {
             return c - uint8(bytes1("0"));
         }
@@ -33,9 +30,9 @@ library Tools {
     // https://stackoverflow.com/questions/69551020/trying-to-convert-address-string-to-type-address-in-solidity
     function hexStringToAddress(
         string memory s
-    ) public pure returns (bytes memory) {
+    ) internal pure returns (bytes memory) {
         bytes memory ss = bytes(s);
-        require(ss.length % 2 == 0); // length must be even
+        require(ss.length % 2 == 0, "string with hex encoded data should have even length"); // length must be even
         bytes memory r = new bytes(ss.length / 2);
         for (uint i = 0; i < ss.length / 2; ++i) {
             r[i] = bytes1(
